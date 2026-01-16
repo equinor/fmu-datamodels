098f0e11d570b9e1726b73e5259659f69d7be9f2
mferrera <13275515+mferrera@users.noreply.github.com>
2026-01-16 13:54:23 +0100

    MAINT: Rename parameter metadata classes

--- tests/test_models/test_ert_parameters.py
+++ tests/test_models/test_ert_parameters.py
@@ -1,19 +1,19 @@
 import pyarrow as pa
 import pytest
 
-from fmu.datamodels.parameters.distributions import (
-    ConstDistribution,
-    DerrfDistribution,
-    DistributionMetadata,
-    DUnifDistribution,
-    ErrfDistribution,
-    LogNormalDistribution,
-    LogUnifDistribution,
-    NormalDistribution,
-    RawDistribution,
-    TriangularDistribution,
-    TruncatedNormalDistribution,
-    UniformDistribution,
+from fmu.datamodels.parameters import (
+    ConstParameter,
+    DerrfParameter,
+    DUnifParameter,
+    ErrfParameter,
+    LogNormalParameter,
+    LogUnifParameter,
+    NormalParameter,
+    ParameterMetadata,
+    RawParameter,
+    TriangularParameter,
+    TruncatedNormalParameter,
+    UniformParameter,
 )
 
 
@@ -21,7 +21,7 @@ def test_to_pa_metadata_serialization() -> None:
     """Dumping Parquet schemas serializes to dict[bytes, bytes].
 
     Uses DUnif because it has both float and int."""
-    dist = DUnifDistribution(
+    dist = DUnifParameter(
         min=0.0, max=1.0, steps=100, group="GLOBVAR", input_source="sampled"
     )
     assert dist.to_pa_metadata() == {
@@ -45,38 +45,38 @@ def test_to_pa_metadata_serialization() -> None:
 
 
 @pytest.mark.parametrize(
-    "distribution_class, value_dict",
+    "parameter_class, value_dict",
     [
-        (UniformDistribution, {"min": 0.0, "max": 1.0}),
-        (LogUnifDistribution, {"min": 0.0, "max": 1.0}),
-        (NormalDistribution, {"mean": 0.2, "std": 0.1}),
+        (UniformParameter, {"min": 0.0, "max": 1.0}),
+        (LogUnifParameter, {"min": 0.0, "max": 1.0}),
+        (NormalParameter, {"mean": 0.2, "std": 0.1}),
         (
-            TruncatedNormalDistribution,
+            TruncatedNormalParameter,
             {"min": 0.0, "max": 1.0, "mean": 0.2, "std": 0.1},
         ),
-        (LogNormalDistribution, {"mean": 0.2, "std": 0.1}),
-        (RawDistribution, {}),
-        (ConstDistribution, {"value": 2.0}),
-        (TriangularDistribution, {"min": 0.0, "max": 3.0, "mode": 2.0}),
-        (ErrfDistribution, {"min": 0.0, "max": 3.0, "skewness": 2.0, "width": 10.0}),
+        (LogNormalParameter, {"mean": 0.2, "std": 0.1}),
+        (RawParameter, {}),
+        (ConstParameter, {"value": 2.0}),
+        (TriangularParameter, {"min": 0.0, "max": 3.0, "mode": 2.0}),
+        (ErrfParameter, {"min": 0.0, "max": 3.0, "skewness": 2.0, "width": 10.0}),
         (
-            DerrfDistribution,
+            DerrfParameter,
             {"min": 0.0, "max": 3.0, "skewness": 2.0, "width": 10.0, "steps": 1000.0},
         ),
-        (DUnifDistribution, {"min": 0.0, "max": 1.0, "steps": 100}),
+        (DUnifParameter, {"min": 0.0, "max": 1.0, "steps": 100}),
     ],
 )
 def test_from_pa_metadata_roundtrip(
-    distribution_class: DistributionMetadata, value_dict: dict[str, int | float]
+    parameter_class: ParameterMetadata, value_dict: dict[str, int | float]
 ) -> None:
-    """Roundtrip serialization-deserialization works for all distributions."""
+    """Roundtrip serialization-deserialization works for all parameters."""
     input_source = (
-        "design_matrix" if type(distribution_class) is RawDistribution else "sampled"
+        "design_matrix" if type(parameter_class) is RawParameter else "sampled"
     )
-    dist = distribution_class.model_validate(
+    dist = parameter_class.model_validate(
         {"group": "GLOBVAR", "input_source": input_source, **value_dict}
     )
     field = pa.field("PARAM1", pa.float64(), metadata=dist.to_pa_metadata())
     assert field.metadata is not None
-    from_dist = distribution_class.from_pa_metadata(field.metadata)
+    from_dist = parameter_class.from_pa_metadata(field.metadata)
     assert dist == from_dist
